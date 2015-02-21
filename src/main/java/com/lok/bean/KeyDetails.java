3b1952023d27c2b6f63a3dd6b9b46a0cbf1ce9c6
USER <deepansh1987@gmail.com>
2015-02-21 00:21:23 -0500

    Set up for rest to get key details.

---
+++ src/main/java/com/lok/bean/KeyDetails.java
@@ -0,0 +1,5 @@
+package com.lok.bean;
+
+public class KeyDetails {
+
+}
---
+++ src/main/java/com/lok/rest/lockerservice/CustomerMasterService.java
@@ -0,0 +1,30 @@
+package com.lok.rest.lockerservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import com.lok.bean.KeyDetails;
+
+@Path("/lockerservice")
+public class CustomerMasterService {
+
+	/**
+	 * Return booking details in the form of
+	 * json
+	 * key would be mapped to the field names to 
+	 * populate correct field. 
+	 */
+	@GET
+	@Path("/keydetails/{keynum}")
+	public KeyDetails getKeyDetails(@PathParam("keynum") String keyNum){
+		
+		KeyDetails keyRecord = null;
+		try{
+			
+		}catch(Exception e){
+			//log to the logger
+		}
+		return keyRecord;
+	}
+}
